d43c7c5b92a0eb7cd83c40c8a48d25b88ef43d43
julius278 <julius.lauterbach@t-online.de>
2022-08-24 14:03:09 +0200

    implemented map based services

---
+++ pet-clinic-data/src/main/java/com/julius/petclinic/services/map/PetMapService.java
@@ -0,0 +1,37 @@
+package com.julius.petclinic.services.map;
+
+import com.julius.petclinic.model.Owner;
+import com.julius.petclinic.model.Pet;
+import com.julius.petclinic.services.CrudService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetMapService extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
