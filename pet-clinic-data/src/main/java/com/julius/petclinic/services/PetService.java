d2dcae6bf3452934db0a1af33dac17df4db48c7d
julius278 <julius.lauterbach@t-online.de>
2022-08-24 11:37:30 +0200

    refactored services to a common interface

--- pet-clinic-data/src/main/java/com/julius/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/julius/petclinic/services/PetService.java
@@ -2,15 +2,5 @@ package com.julius.petclinic.services;
 
 import com.julius.petclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    //Pet findByOwnerId(Long ownerId);
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long>{
 }
