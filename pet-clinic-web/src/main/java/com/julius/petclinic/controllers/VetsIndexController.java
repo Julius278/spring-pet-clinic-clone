eeb602c52284854b232fd8578c7fe63c38a3c7fd
julius278 <julius.lauterbach@t-online.de>
2022-08-29 10:00:14 +0200

    list all vets on vet index page. closes #8

--- pet-clinic-web/src/main/java/com/julius/petclinic/controllers/VetsIndexController.java
+++ pet-clinic-web/src/main/java/com/julius/petclinic/controllers/VetsIndexController.java
@@ -1,13 +1,22 @@
 package com.julius.petclinic.controllers;
 
+import com.julius.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/vets")
 @Controller
 public class VetsIndexController {
 
-    @RequestMapping("vets")
-    public String getVets(){
+    private final VetService vetService;
+
+    public VetsIndexController(VetService vetService){
+        this.vetService = vetService;
+    }
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String getVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
