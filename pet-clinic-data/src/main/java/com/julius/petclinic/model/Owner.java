1bb52f986e40105f053fde16226c04ba15a24d92
julius278 <julius.lauterbach@t-online.de>
2022-09-21 14:29:07 +0200

    added JPA annotations to Owner, closes #28

--- pet-clinic-data/src/main/java/com/julius/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/julius/petclinic/model/Owner.java
@@ -1,13 +1,23 @@
 package com.julius.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String City;
+
+    @Column(name = "telephone")
     private String telephone;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
     public Set<Pet> getPets() {
