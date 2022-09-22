598f3471459f5dbef92e5162d2cd0ad0e977e50e
julius278 <julius.lauterbach@t-online.de>
2022-09-22 14:20:04 +0200

    added a PetJpaService as PetService Impl, closes #36

---
+++ pet-clinic-data/src/main/java/com/julius/petclinic/services/springdatajpa/PetJpaService.java
@@ -0,0 +1,54 @@
+package com.julius.petclinic.services.springdatajpa;
+
+import com.julius.petclinic.model.Pet;
+import com.julius.petclinic.repositories.PetRepository;
+import com.julius.petclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        Optional<Pet> optionalPet = petRepository.findById(aLong);
+        if(optionalPet.isEmpty()){
+            return null;
+        }
+        Pet pet = optionalPet.get();
+        return pet;
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
