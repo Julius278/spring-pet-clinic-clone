8ddd9cb9f17341f8282a6aaab29ba130da02659e
julius278 <julius.lauterbach@t-online.de>
2022-09-23 14:56:55 +0200

    added VisitMapService, closes #45

---
+++ pet-clinic-data/src/main/java/com/julius/petclinic/services/map/VisitMapService.java
@@ -0,0 +1,33 @@
+package com.julius.petclinic.services.map;
+
+import com.julius.petclinic.model.Visit;
+import com.julius.petclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
